f8ad0e56fb3d19ebfb9b36baa7a81a1fa9e0c7f1
Ajasf444 <longstephen444@gmail.com>
2025-04-30 07:24:35 -0500

    added some class members

--- src/beaker.py
+++ src/beaker.py
@@ -1,7 +1,16 @@
 from game_parameters import BEAKER_CAPACITY
+from liquid import Liquid
 
 
 class Beaker:
-    def __init__(self, contents=None):
-        self.contents = contents if contents else []
-        self.capacity = BEAKER_CAPACITY
+    def __init__(self, contents: list[Liquid] = None):
+        self._contents = contents if contents else []
+        self._capacity = BEAKER_CAPACITY
+        self._occupied_volume = (
+            None  # TODO: account for the amount of liquid in contents and capacity
+        )
+        self._volume_remaining = None  # TODO: account for occupied_volume
+
+
+if __name__ == "__main__":
+    pass
