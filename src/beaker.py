6fe9f0d1892e1b08d92b64acb505fa7c2ecefd28
Ajasf444 <longstephen444@gmail.com>
2025-04-30 08:27:31 -0500

    completed TODOs for Beaker class members

--- src/beaker.py
+++ src/beaker.py
@@ -6,10 +6,21 @@ class Beaker:
     def __init__(self, contents: list[Liquid] = None):
         self._contents = contents if contents else []
         self._capacity = BEAKER_CAPACITY
-        self._occupied_volume = (
-            None  # TODO: account for the amount of liquid in contents and capacity
-        )
-        self._volume_remaining = None  # TODO: account for occupied_volume
+        self._occupied_volume = self._calculate_volume()
+        if self._occupied_volume > self._capacity:
+            raise Exception("Volume of contents exceeds beaker capacity.")
+        self._volume_remaining = self._get_volume_remaining()
+
+    def _calculate_volume(self):
+        if not self._contents:
+            return 0
+        volume = 0
+        for liquid in self._contents:
+            volume += liquid.get_amount()
+        return volume
+
+    def _get_volume_remaining(self):
+        return self._capacity - self._occupied_volume
 
 
 if __name__ == "__main__":
