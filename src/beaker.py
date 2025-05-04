857614e13aa10e2e160960553e46b1acb8099469
Ajasf444 <longstephen444@gmail.com>
2025-05-04 15:10:10 -0500

    refactored pour()

--- src/beaker.py
+++ src/beaker.py
@@ -7,10 +7,11 @@ class Beaker:
     def __init__(self, contents: list[Liquid] = None) -> None:
         self._contents = contents if contents else []
         self._capacity = BEAKER_CAPACITY
-        self._occupied_volume = self._calculate_volume()
+        self._occupied_volume = None
+        self._volume_remaining = None
+        self._update()
         if not self._volume_is_legal():
             raise Exception("Volume of contents exceeds beaker capacity.")
-        self._volume_remaining = self._calculate_volume_remaining()
 
     def __repr__(self):
         if self.is_empty():
@@ -20,17 +21,21 @@ class Beaker:
         )
         return contents
 
+    def _append_liquid(self, liquid: Liquid = None) -> None:
+        self._contents.append(liquid)
+        self._update()
+
     def add_liquid(self, liquid: Liquid = None) -> None:
-        if liquid:
+        if self.is_empty():
             self._contents.append(liquid)
-            self._update()
+        else:
+            self._contents[-1] += liquid
+        self._update()
 
     def pour(self, dest: "Beaker") -> bool:
-        if dest.is_empty():
+        if self._valid_pour(dest):
             liquid = self._extract_liquid()
             dest.add_liquid(liquid)
-            return True
-        if self._valid_pour(dest):
             return True
         return False
 
@@ -64,6 +69,8 @@ class Beaker:
         return self._volume_remaining
 
     def _get_top_color(self) -> Color:
+        if not self._contents:
+            return None
         return self._contents[-1].get_color()
 
     def _get_top_volume(self) -> int:
@@ -74,7 +81,7 @@ class Beaker:
         dest_color = dest._get_top_color()
         colors_match = start_color == dest_color
         dest_enough_volume = dest._get_volume_remaining() >= start_volume
-        return colors_match and dest_enough_volume
+        return colors_match and dest_enough_volume or dest.is_empty()
 
 
 if __name__ == "__main__":
@@ -84,13 +91,25 @@ if __name__ == "__main__":
     beaker_1 = Beaker()
     print(beaker_0)
     print(beaker_0._occupied_volume, beaker_0._volume_remaining)
-    beaker_0.add_liquid(liquid_0)
+    print("Adding liquid_0 to beaker_0")
+    beaker_0._append_liquid(liquid_0)
     print(beaker_0._occupied_volume, beaker_0._volume_remaining)
     print(beaker_0)
-    beaker_0.add_liquid(liquid_1)
+    print("Adding liquid_1 to beaker_0")
+    beaker_0._append_liquid(liquid_1)
     print(beaker_0._occupied_volume, beaker_0._volume_remaining)
     print(beaker_0)
     print(beaker_1)
+    print("Pouring beaker_0 into beaker_1")
+    beaker_0.pour(beaker_1)
+    print(beaker_0)
+    print(beaker_0._occupied_volume, beaker_0._volume_remaining)
+    print(beaker_1)
+    print(beaker_1._occupied_volume, beaker_1._volume_remaining)
+    print("Adding liquid_1 to beaker_0")
+    beaker_0._append_liquid(liquid_1)
+    print(beaker_0)
+    print(beaker_0._occupied_volume, beaker_0._volume_remaining)
     beaker_0.pour(beaker_1)
     print(beaker_0)
     print(beaker_0._occupied_volume, beaker_0._volume_remaining)
